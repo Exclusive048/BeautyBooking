62b88ddaaa3b6357b3fdf0c556cc37dd01a54c02
Artem Dmitriev <dmitriev_ar@nlmk.com>
2026-02-24 20:54:01 +0300

    delete legacy

--- src/features/auth/use-current-account.ts
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { currentAccountMock } from "./mock";
-
-export function useCurrentAccount() {
-  // позже здесь будет запрос /session или next-auth
-  return currentAccountMock;
-}
